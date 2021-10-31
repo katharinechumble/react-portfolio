f38ff3b2fd52ea3abb916d51a7e4e283749e87a5
Katharine C Humble <katharinechumble@gmail.com>
2021-10-31 10:01:41 -0400

    updated about section

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      <div className="my-2">
+        <p>
+          I am a junior full stack developer based in Louisville, KY, USA. I
+          have experience with implementing both front and back ends for modern
+          websites. Please feel free to contact me with questions about me or my
+          work.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
