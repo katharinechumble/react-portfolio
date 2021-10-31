41345b86851f4813f89aaa972e5fb82d7b835ebb
Katharine C Humble <katharinechumble@gmail.com>
2021-10-30 21:55:47 -0400

    added header

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Navigation from "./Navigation";
+import About from "./About";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+
+function Header() {
+    const [currentPage, handlePageChange] = useState("About");
+
+    const renderPage = () => {
+        switch(currentPage) {
+            case "About":
+                return <About />;
+            case "Contact":
+                return <Contact />;
+            case "Portfolio":
+                return <Portfolio />;
+
+            default:
+                return <About />;
+        }
+    }
+
+    return (
+        <div>
+            <nav className="navbar">
+                <a className="navbar-item" href="https://github.com/katharinechumble">
+                    <span className="title-text">Katharine C Humble</span>
+                </a>
+            </nav>
+            <Navigation
+                currentPage={ currentPage }
+                handlePageChange={ handlePageChange } />
+            <main>
+                <div>{ renderPage(currentPage) }</div>
+            </main>
+        </div>
+    );
+}
+
+export default Header;
