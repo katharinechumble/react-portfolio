1c35684b090342424f28b27dc85e5c6293efef63
Katharine C Humble <katharinechumble@gmail.com>
2021-10-31 10:59:56 -0400

    added footer

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="content has-text-centered">
+               <p>
+                   <a href="https://www.linkedin.com/in/katharine-humble-45906012b/">LinkedIn</a>
+                   .
+                   <a href="https://github.com/katharinechumble">GitHub</a>
+                   .
+                   <a href="mailto:katharinechumble@gmail.com">Email</a>
+                   </p> 
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
